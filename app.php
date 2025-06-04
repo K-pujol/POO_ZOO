adf31e064a5376ec3e7eaf113d568458239e4fe8
K-pujol <killianpujol26@gmail.com>
2025-06-04 10:57:54 +0200

    fin étape 2 création classe animal

--- app.php
+++ app.php
@@ -2,3 +2,30 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\App;
+
+abstract class Animal
+{
+    // Attribut privé
+    private string $name;
+
+    // Constructeur : initialise l'attribut $name
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    // méthode
+    // Accesseur (getter) : retourne la valeur de $name
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    abstract protected function getNoise(): string;
+
+    public function noise (): string
+    {
+        return $this->getName() . ' fait ' . $this->getNoise();
+    }
+
+}
